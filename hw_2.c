9ed0bfa5fb284ccb25957b67a93496880422f1ff
daniel940212 <u11202225@o365.ttu.edu.tw>
2024-05-02 17:10:55 +0800

    宣告變數

--- hw_2.c
+++ hw_2.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>  
+
 int main()
 {
-    int a;
-    
-    return 0;
+    int password = 2024; 
+    int input;           
+    int attempt = 0;     
+    int choice;
+    const int max_attempts = 3;
 }
