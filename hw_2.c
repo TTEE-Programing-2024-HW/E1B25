0733c64938773f6946793f4d94a7b8b04e4d7fe1
daniel940212 <u11202225@o365.ttu.edu.tw>
2024-05-03 10:39:59 +0800

    印出表格

--- hw_2.c
+++ hw_2.c
@@ -17,3 +17,27 @@ int main()
     system("pause");
     system("cls");
 }
+while (attempt < max_attempts)
+    {
+        printf("請輸入四位數字的密碼：");
+        fflush(stdin);
+        scanf("%d", &pw_input);
+        
+        if (pw_input == pass)
+        {
+            while (1)
+            {
+                system("cls");
+                printf(" -----------------------------------------\n");
+                printf("|            a. 畫出直角三角形            |\n");
+                printf("|                                         |\n");
+				printf("|             b. 顯示乘法表               |\n");
+                printf("|                                         |\n");
+				printf("|                c. 結束                  |\n");
+                printf(" -----------------------------------------\n");
+                
+                printf("輸入A、B、C以繼續，不分大小寫：");
+                choice = getch();
+            }
+        }     
+    }        
