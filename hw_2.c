feefcf381660ab861ac1e9237b7b704bb50f0aba
daniel940212 <u11202225@o365.ttu.edu.tw>
2024-05-03 10:33:33 +0800

    更新版#1

--- hw_2.c
+++ hw_2.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <conio.h>  
+#include <conio.h>
 
 int main()
 {
-    int password = 2024; 
-    int input;           
-    int attempt = 0;     
+    int password = 2024;
+    int input;
+    int attempt = 0;
     int choice;
     const int max_attempts = 3;
 
-
-    for(int i=1;i<20;i++)
+    for (int i = 1; i < 20; i++)
     {
-    	 printf("Hello\n");
-	}
-	system("pause");
+        printf("Hello\n");
+    }
+    system("pause");
     system("cls");
 }
