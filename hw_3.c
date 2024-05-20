a36e0622ccfecf143c9254731697d9ba0c177cce
daniel940212 <u11202225@o365.ttu.edu.tw>
2024-05-20 18:31:50 +0800

    新增錯誤次數和密碼

--- hw_3.c
+++ hw_3.c
@@ -15,6 +15,14 @@ void displaySeats();
 void arrangeSeats();
 void chooseSeats();
 int checkAvailability(int row, int col, int count);
+int main() {
+    int pass = 2024; // 正確的密碼
+    int pw_input;    // 使用者輸入的密碼
+    int attempt = 0; // 嘗試次數
+    int choice;      // 使用者選擇
+    const int max_attempts = 3;  // 允許的最大嘗試次數
+    
+    // 個性化畫面與密碼輸入
   for(int i = 0; i < 20; i++) {
         printf("Hello!\n");
     }
