f9f7caf992d6471f526a261209bc229f21db1a92
E1B25 <u11202225@o365.ttu.edu.tw>
2024-05-30 18:19:46 +0800

    A功能完成

--- HW#4/hw_4.c
+++ HW#4/hw_4.c
@@ -60,3 +60,66 @@ void welcome_screen() {
     printf("嘗試次數過多。程序將退出。\n");
     exit(1);
 }
+// 函數顯示主選單
+void display_menu() {
+    printf("------------[成績管理系統]----------\n");
+    printf("|  a. 輸入學生成績         |\n");
+    printf("|  b. 顯示學生成績         |\n");
+    printf("|  c. 搜索學生成績         |\n");
+    printf("|  d. 成績排名             |\n");
+    printf("|  e. 退出系統             |\n");
+    printf("------------------------------------\n");
+}
+
+// 函數輸入學生成績
+void enter_student_grades(Student students[], int *student_count) {
+    int n;
+
+    clear_screen();
+    printf("輸入學生數量 (5-10): ");
+    
+    // 驗證學生數量
+    while (scanf("%d", &n) != 1 || n < MIN_STUDENTS || n > MAX_STUDENTS) {
+        printf("輸入無效。請輸入5到10之間的數字: ");
+        while (getchar() != '\n'); // 清除輸入緩衝區
+    }
+
+    // 循環輸入每個學生的詳細信息
+    for (int i = 0; i < n; i++) {
+        printf("輸入第 %d 個學生姓名: ", i + 1);
+        scanf("%s", students[i].name);
+
+        printf("輸入第 %d 個學生學號 (6位數字): ", i + 1);
+        // 驗證學生學號
+        while (scanf("%d", &students[i].student_id) != 1 || students[i].student_id < 100000 || students[i].student_id > 999999) {
+            printf("學號無效。請輸入6位數字: ");
+            while (getchar() != '\n'); // 清除輸入緩衝區
+        }
+
+        printf("輸入數學成績 (0-100): ");
+        // 驗證數學成績
+        while (scanf("%d", &students[i].math) != 1 || students[i].math < 0 || students[i].math > 100) {
+            printf("成績無效。請輸入0到100之間的數字: ");
+            while (getchar() != '\n'); // 清除輸入緩衝區
+        }
+
+        printf("輸入物理成績 (0-100): ");
+        // 驗證物理成績
+        while (scanf("%d", &students[i].physics) != 1 || students[i].physics < 0 || students[i].physics > 100) {
+            printf("成績無效。請輸入0到100之間的數字: ");
+            while (getchar() != '\n'); // 清除輸入緩衝區
+        }
+
+        printf("輸入英文成績 (0-100): ");
+        // 驗證英文成績
+        while (scanf("%d", &students[i].english) != 1 || students[i].english < 0 || students[i].english > 100) {
+            printf("成績無效。請輸入0到100之間的數字: ");
+            while (getchar() != '\n'); // 清除輸入緩衝區
+        }
+
+        // 計算平均成績
+        students[i].average = (students[i].math + students[i].physics + students[i].english) / 3.0;
+    }
+
+    *student_count = n; // 更新學生數量
+}
